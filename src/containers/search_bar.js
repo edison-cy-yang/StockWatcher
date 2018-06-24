815460c871b04bfb77d047c07952b4ea567819c2
Edison Yang <edison.cy.yang@gmail.com>
2018-06-23 23:33:04 -0700

    created search bar state

--- src/containers/search_bar.js
+++ src/containers/search_bar.js
@@ -1,10 +1,36 @@
 import React, { Component } from 'react';
 
 export default class SearchBar extends Component {
+    //initialize component state
+    constructor(props) {
+        super(props);
+
+        this.state = { term: ''};
+        this.onInputChange = this.onInputChange.bind(this);
+    }
+
+    onInputChange(event) {
+        this.setState({term: event.target.value});
+    }
+
+    onFormSubmit(event) {
+        event.preventDefault();
+
+        //we need to go and fetch stock data
+    }
+
     render() {
         return (
-            <form className="input-group">
-                <input />
+            <form onSubmit={this.onFormSubmit} className="input-group">
+                <input 
+                    placeholder="Get information about your stocks"
+                    className="form-control"
+                    value={this.state.term}
+                    onChange={this.onInputChange}
+                />
+                <span className="input-group-btn">
+                    <button type="submit" className="btn btn-secondary">Submit</button>
+                </span>
             </form>
         )
     }
