561d4ba6c122c8d484983d7bc83fd3ea8fb9b0b2
Edison Yang <edison.cy.yang@gmail.com>
2018-06-24 00:36:54 -0700

    set up axios API request in action creator

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,9 +1,17 @@
-const API_KEU = "UGXPUCUDFIHWVTF0";
+import axios from 'axios';
+
+const API_KEY = "UGXPUCUDFIHWVTF0";
+
+const ROOT_URL = `https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&apikey=${API_KEY}`;
 
 export const FETCH_STOCK = 'FETCH_STOCK';
 
-export function fetchStock() {
+export function fetchStock(stock) {
+    const url = `${ROOT_URL}&symbol=${stock}`;
+    const request = axios.get(url);
+
     return {
-        type: FETCH_STOCK
+        type: FETCH_STOCK,
+        payload: request
     };
 }
--- src/containers/search_bar.js
+++ src/containers/search_bar.js
@@ -1,12 +1,16 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
+import { fetchStock } from '../actions/index';
 
-export default class SearchBar extends Component {
+class SearchBar extends Component {
     //initialize component state
     constructor(props) {
         super(props);
 
         this.state = { term: ''};
         this.onInputChange = this.onInputChange.bind(this);
+        this.onFormSubmit = this.onFormSubmit.bind(this);
     }
 
     onInputChange(event) {
@@ -17,6 +21,8 @@ export default class SearchBar extends Component {
         event.preventDefault();
 
         //we need to go and fetch stock data
+        this.props.fetchStock(this.state.term);
+        this.setState({ term: ''});
     }
 
     render() {
@@ -35,3 +41,10 @@ export default class SearchBar extends Component {
         )
     }
 }
+
+//when action creator is called, bindActionCreators makes sure action flows down into middleware and reducer
+function mapDispatchToProps(dispatch) {
+    return bindActionCreators({fetchStock}, dispatch);
+}
+
+export default connect(null, mapDispatchToProps)(SearchBar);
