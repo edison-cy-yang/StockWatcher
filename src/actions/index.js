6eeb7be1180d904ed50352a60063fc8dff6db67d
Edison Yang <edison.cy.yang@gmail.com>
2018-06-26 23:42:34 -0700

    slight code refactoring

--- src/actions/index.js
+++ src/actions/index.js
@@ -2,12 +2,16 @@ import axios from 'axios';
 
 const API_KEY = "UGXPUCUDFIHWVTF0";
 
-const ROOT_URL = `https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&apikey=${API_KEY}`;
+const DAILY = 'TIME_SERIES_DAILY';
+const WEEKLY = "TIME_SERIES_WEEKLY";
+const MONTHLY = 'TIME_SERIES_MONTHLY';
+
+const ROOT_URL = `https://www.alphavantage.co/query?apikey=${API_KEY}`;
 
 export const FETCH_STOCK = 'FETCH_STOCK';
 
 export function fetchStock(stock) {
-    const url = `${ROOT_URL}&symbol=${stock}`;
+    const url = `${ROOT_URL}&symbol=${stock}&function=${DAILY}`;
     const request = axios.get(url);
 
     console.log('Request: ', request);
