66cd8c6d4b7869ce2f33916bce419a1f94957217
Edison Yang <edison.cy.yang@gmail.com>
2018-07-02 17:11:27 -0700

    Passport GoogleStrategy

--- index.js
+++ index.js
@@ -1,14 +1,33 @@
 const express = require('express');
 const passport = require('passport');
 const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const keys = require('./config/keys');
 
 const app = express();
 
-// passport.use(new GoogleStrategy());
+//set up passport with google API ID
+passport.use(new GoogleStrategy({
+                clientID: keys.googleCLientID,
+                clientSecret: keys.googleClientSecret,
+                callbackURL: '/auth/google/callback'
+            },
+            (accessToken, refreshToken, profile, done) => {
+                console.log(accessToken);
+                console.log(refreshToken);
+                console.log(profile);
+            })
+);
 
-app.get('/', (req, res) => {
-    res.send({test: 'test'});
-});
+//authenticate the user with oath callback into the oauth flow
+//ask for user profile and email
+app.get(
+    '/auth/google',
+    passport.authenticate('google', {
+        scope: ['profile', 'email']
+    })
+);
+
+app.get('/auth/google/callback', passport.authenticate('google'))
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
