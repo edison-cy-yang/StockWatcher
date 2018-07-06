772ca9d7913e8927ac31904f640816b4243cc3ae
Edison Yang <edison.cy.yang@gmail.com>
2018-07-05 22:38:38 -0700

    google strategy setting proxy true

--- services/passport.js
+++ services/passport.js
@@ -23,7 +23,8 @@ passport.use(
     new GoogleStrategy({
         clientID: keys.googleClientID,
         clientSecret: keys.googleClientSecret,
-        callbackURL: '/auth/google/callback'
+        callbackURL: '/auth/google/callback',
+        proxy: true //fine to walk through any proxy
         },
         (accessToken, refreshToken, profile, done) => {
             User.findOne({googleId: profile.id}).then(existingUser => {
