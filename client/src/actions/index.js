169b700da995d4adc5b32b920f83f7c6ef084655
Edison Yang <edison.cy.yang@gmail.com>
2018-07-10 00:15:31 -0700

    unused const

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -1,14 +1,6 @@
 import axios from 'axios';
 import { FETCH_USER, FETCH_STOCK } from './types';
 
-const ROOT_URL = 'https://api.iextrading.com/1.0/';
-const STOCK = 'stock';
-const DAILY = '1d';
-const MONTHLY = '1m';
-const YEARLY = '1y';
-
-const CHART = 'chart';
-
 export const fetchStock = async (symbol) => {
     const res = await axios.post('/api/newstock', {symbol});
 
